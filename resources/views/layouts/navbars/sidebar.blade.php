9f8c64426165116b0c0a3056d2790338bbb5309d
firliiaf <105795807+firliiaf@users.noreply.github.com>
2024-11-19 14:54:04 +0700

    first commit

---
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -0,0 +1,43 @@
+<div class="sidebar" data-color="orange">
+  <!--
+    Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+-->
+  <div class="logo">
+    <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+      {{ __('CT') }}
+    </a>
+    <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+      {{ __('Creative Tim') }}
+    </a>
+  </div>
+  <div class="sidebar-wrapper" id="sidebar-wrapper">
+    <ul class="nav">
+      <li class="@if ($activePage == 'home') active @endif">
+        <a href="{{ route('home') }}">
+          <i class="now-ui-icons design_app"></i>
+          <p>{{ __('Dashboard') }}</p>
+        </a>
+      </li>
+      <li>
+
+        <div class="collapse show" id="laravelExamples">
+          <ul class="nav">
+            <li class="@if ($activePage == 'profile') active @endif">
+              <a href="{{ route('profile.edit') }}">
+                <i class="now-ui-icons users_single-02"></i>
+                <p> {{ __("User Profile") }} </p>
+              </a>
+            </li>
+            <li class="@if ($activePage == 'users') active @endif">
+              <a href="{{ route('user.index') }}">
+                <i class="now-ui-icons design_bullet-list-67"></i>
+                <p> {{ __("User Management") }} </p>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+
+    </ul>
+  </div>
+</div>
